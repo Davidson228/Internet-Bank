a98bff5a0b32396145f6e587a15f65089f41f604
Davidson228 <davidarinsh@gmail.com>
2019-01-12 11:01:40 +0200

    test

--- new_account.php
+++ new_account.php
@@ -2,13 +2,13 @@
 require 'connection.php';
 
 $a = 'LV42wert';
-for ($i = 1; $i = 13; $i++) {
+for ($i = 1; $i <= 13; $i++) {
    $a = $a.rand(0,9);
 }
 $b_account = new Bank_account($db);
-$b_account->clients_id = $_SESSION['clients_id'];
-$b_account->account_number = $a;
-$b_account->balance = 0;
+$b_account['clients_id'] = $_SESSION['clients_id'];
+$b_account['account_number'] = $a;
+$b_account['balance'] = 0;
 $b_account->save();
 
 header('Location: bank_account.php');

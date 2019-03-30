e5fd10b5d461c4dee87569f471a0ca5c15b59ec1
Davidson228 <31989135+Davidson228@users.noreply.github.com>
2019-03-30 09:47:08 +0200

    few

--- ban.php
+++ ban.php
@@ -3,4 +3,7 @@ require 'connection.php';
 $app = new \atk4\ui\App('BAN');
 $app->initLayout('Centered');
 $app->add(['Message','Na ban']);
- 
+
+$button3 = $app->add('Button');
+$button3->set('Logout');
+$button3->link('bank_account.php');
